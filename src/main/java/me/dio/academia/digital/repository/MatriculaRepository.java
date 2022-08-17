76cdacd9fdbee3d7152e86254547e826b9d5211d
Raoni Cerqueira <raoni_henrique@live.com>
2022-08-17 16:11:03 -0300

    Primeiro Commit

---
+++ src/main/java/me/dio/academia/digital/repository/MatriculaRepository.java
@@ -0,0 +1,11 @@
+package me.dio.academia.digital.repository;
+
+import me.dio.academia.digital.entity.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+
+    List<Matricula> findByAlunoBairro(String bairro);
+}
